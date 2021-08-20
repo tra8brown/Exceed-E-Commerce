c30f52acf1920e2f0c0de812da52c71f92d32531
TraBee <tra8brown@gmail.com>
2021-08-20 12:59:03 -0600

    tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -20,20 +20,80 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+    Tag.findOne({
+
+            where: {
+                id: req.params.id
+            },
+            include: [{
+                model: Product,
+                attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+            }]
+
+        })
+        .then(tags => res.json(tags))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
     // find a single tag by its `id`
     // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
     // create a new tag
+    Tag.create({
+            tag_name: req.body.tag_name
+        })
+        .then(tags => res.json(tags))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err)
+        });
 });
 
 router.put('/:id', (req, res) => {
     // update a tag's name by its `id` value
+    Tag.update({
+            tag_name: req.body.tag_name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(tags => {
+            if (!tags) {
+                res.status(404).json({ message: 'No tag found with this id ' });
+                return;
+            }
+            res.json(tags);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.delete('/:id', (req, res) => {
     // delete on tag by its `id` value
+    Tag.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(tags => {
+            if (!tags) {
+                res.status(404).json({ message: 'No tag found with this id' });
+                return;
+            }
+            res.json(tags);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
+module.exports = router;
 
 module.exports = router;
